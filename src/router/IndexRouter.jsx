3b61f4b680323a12504ba93f77cd900607a02119
Heisenberg <1157109110@qq.com>
2022-03-13 22:02:30 +0800

    delete

--- src/router/IndexRouter.jsx
+++ src/router/IndexRouter.jsx
@@ -1,9 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import { Spin } from 'antd'
-import TagTest from "../views/TagTest";
-import DrawerTest from "../views/DrawerTest";
-import ComponentsTest from "../views/ComponentsTest";
 
 // import Detail from "../views/news/Detail/Detail";
 // import NewsList from "../views/news/NewsList/NewsList";
@@ -14,7 +11,6 @@ const Login = lazy(() => import("../views/Login/Login"));
 const Detail = lazy(() => import("../views/news/Detail/Detail"));
 const NewsList = lazy(() => import("../views/news/NewsList/NewsList"));
 const NewsSandBox = lazy(() => import("../views/sandbox/NewsSandBox"));
-const SelectTest = lazy(() => import("../views/SelectTest"));
 
 export default function IndexRouter() {
   return (
@@ -24,10 +20,6 @@ export default function IndexRouter() {
           <Route path="/login" component={Login} />
           <Route path="/news" component={NewsList} />
           <Route path="/detail/:id" component={Detail} />
-          <Route path="/select" component={SelectTest} />
-          <Route path="/tag" component={TagTest} />
-          <Route path="/Drawer" component={DrawerTest} />
-          <Route path="/test" component={ComponentsTest} />
           <Route
             path="/"
             render={() =>
